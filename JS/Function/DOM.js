4d7f934e76f80963c9b8cafd95cc3b1d1615c774
shudhanshu mishra <shudhanshu.webdev23@gmail.com>
2026-01-18 15:33:48 +0530

    Implement DOM manipulation functions and update script reference in HTML

--- JS/Function/DOM.js
+++ JS/Function/DOM.js
@@ -1,2 +1,35 @@
 //DOM (Document Object Model) 
 // This module provides functions to manipulate the DOM in a web page.
+//the html dom can be used to acces and manipulate html elements
+
+// Function to get an element by its ID
+function getElementById(id) {
+    return document.getElementById(id);
+}
+// Function to get elements by their class name
+function getElementsByClassName(className) {
+    return document.getElementsByClassName(className);
+}
+// Function to create a new element
+function createElement(tagName) {
+    return document.createElement(tagName);
+}
+//query selector
+function querySelector(selector){
+    return document.querySelector(selector);
+}
+//setting content in object
+function setContent(element, content){
+    element.innerHTML = content;
+}
+//manipulating attributes
+function setAttribute(element, attrName, attrValue){
+    element.setAttribute(attrName, attrValue);
+}
+function getAttribute(element, attrName){
+    return element.getAttribute(attrName);
+}
+//manipulating styles
+function setStyle(element, styleName, styleValue){
+    element.style[styleName] = styleValue;
+}
