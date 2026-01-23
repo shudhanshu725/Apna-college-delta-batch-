fadb78158f86e8b7d78643adc9f88bd5802a0548
shudhanshu mishra <shudhanshu.webdev23@gmail.com>
2026-01-23 16:23:16 +0530

    Add todo list functionality with event handling for adding tasks

---
+++ JS/DOM/miniproject.js
@@ -0,0 +1,57 @@
+// let btn = document.querySelectorAll("button");
+// console.dir(btn);
+// btn.forEach(function(button) {
+//     button.onclick = function() {//onclick is an event handler
+//         alert("Button was clicked!");
+//     }
+//     button.onmouseenter = function() {//onmouseenter is an event handler
+//         alert("Button was hovered!");
+//     }
+// });
+// event listeners
+// let btn = document.querySelectorAll("button");
+// console.dir(btn);
+// btn.forEach(function(button) {
+//     button.addEventListener('dblclick', function() { //addEventListener is an event listener and dblclick is an event type
+//         alert("Button was clicked!");
+//     });
+// });
+//Generate random colors on button click
+// let btn = document.querySelectorAll("button");
+// console.dir(btn);
+// btn.forEach(function(button) {
+//     button.addEventListener('click', function() { //addEventListener is an event listener and click is an event type
+//         let red = Math.floor(Math.random() * 256);
+//         let green = Math.floor(Math.random() * 256);
+//         let blue = Math.floor(Math.random() * 256);
+//         let color = `rgb(${red}, ${green}, ${blue})`;
+//         console.log(color);
+//     });
+// });
+// //Generate random colors on button click and change background color
+// let btns = document.querySelectorAll("button");
+// console.dir(btns);
+// btns.forEach(function(button) {
+//     button.addEventListener('click', function() { //addEventListener is an event listener and click is an event type
+//         let red = Math.floor(Math.random() * 256);
+//         let green = Math.floor(Math.random() * 256);
+//         let blue = Math.floor(Math.random() * 256);
+//         let color = `rgb(${red}, ${green}, ${blue})`;
+//         console.log(color);
+//     });
+// });
+// document.body.style.backgroundColor = color;
+
+// TODO LIST MINI PROJECT
+let btn= document.querySelector("button");
+let ul =document.querySelector("ul");
+let inp = document.querySelector("input");
+
+btn.addEventListener('click',function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    ul.appendChild(item);
+    inp.value="";
+});
+
+
